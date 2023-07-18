7c77082a7b9bb03b81ffb413482e9d0f4343079d
Vitalya <vitalikkmita@gmail.com>
2023-07-18 13:12:00 +0100

    Now you can convert multiple images

--- src/main.rs
+++ src/main.rs
@@ -3,41 +3,53 @@ use std::env;
 use terminal_size::{terminal_size, Height, Width};
 
 fn main() {
+    // collect args
     let args: Vec<String> = env::args().collect();
-    
+
+    // if there is no args print error
     if args.len() <= 1 {
         eprintln!("No arguments");
     } else {
-        let image = image::open(&args[1]).unwrap();
-        let terminal_size = get_terminal_size();
-        let palette: String = " .:-=+*#%@".to_string();
-        //let palette =  r"$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,^`'. ".chars().rev().collect::<String>();
-        let image_size = image.dimensions();
-
-        // calculating cluster size
-        let cluster_width = image_size.0 / terminal_size.0 as u32 * 6;
-        let cluster_height = image_size.0 / terminal_size.0 as u32 * 8;
-        println!("{:?}", terminal_size);
-
-        let mut y = 0;
-        while image_size.1 >= y + cluster_height {
-            let mut x = 0;
-            while image_size.0 >= x + cluster_width {
-                let brightness =
-                    get_brightness_of_cluster(&image, x, y, cluster_width, cluster_height);
-
-                print!("{}", pick_char_from_palette(brightness, 255, &palette));
-                x += cluster_width;
-            }
-            println!();
-            y += cluster_height;
+        for arg in args.iter().skip(1) {
+            convert_to_ascii(&arg);
         }
+    }
+}
+
+fn convert_to_ascii(image_path: &str) {
+    let image = match image::open(image_path) {
+        Ok(image) => image,
+        Err(e) => return eprintln!("{}", e),
+    };
+
+    let terminal_size = get_terminal_size();
+    println!("{:?}", terminal_size);
+
+    let palette: String = " .:-=+*#%@".to_string();
+    //let palette =  r"$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,^`'. ".chars().rev().collect::<String>();
+    let image_size = image.dimensions();
+
+    // calculating cluster size
+    let cluster_width = image_size.0 / terminal_size.0 as u32 * 3;
+    let cluster_height = image_size.1 / terminal_size.1 as u32 * 2;
+
+    let mut y = 0;
+    while image_size.1 >= y + cluster_height {
+        let mut x = 0;
+        while image_size.0 >= x + cluster_width {
+            let brightness = get_brightness_of_cluster(&image, x, y, cluster_width, cluster_height);
+
+            print!("{}", pick_char_from_palette(brightness, 255, &palette));
+            x += cluster_width;
+        }
+        println!();
+        y += cluster_height;
     }
 }
 
 fn pick_char_from_palette(value: usize, max_value: usize, palette: &String) -> char {
     if value > max_value {
-        panic!("The value is higher the max value")
+        panic!("The value is higher than the max value")
     } else {
         let mut char = max_value as f64 / palette.len() as f64;
         char = char.ceil();
@@ -80,43 +92,3 @@ fn get_terminal_size() -> (u16, u16) {
         panic!("Can't get terminal size")
     }
 }
-
-// use clearscreen;
-// use std::{thread, time};
-
-// fn spawn_pyramid(angle:usize, symbol:&str, blank:&str) {
-//     for i in 0..10 {
-//         println!("{}", blank.repeat(i*angle)+&symbol.repeat(i))
-//     }
-// }
-
-// fn main() {
-
-//     const DELAY_IN_MILLS:u64 = 500;
-//     const MAX_ANGLE:usize = 1;
-//     const MIN_ANGLE:usize = 0;
-//     const STEP:usize = 1;
-//     let mut angle:usize = 0;
-//     let mut side:bool = true;
-
-//     loop {
-
-//         spawn_pyramid(angle, "_", "!");
-
-//         if side {
-//             angle = angle + STEP;
-//             if angle > MAX_ANGLE {
-//                 side = false;
-//             }
-//         } else {
-//             angle = angle - STEP;
-//             if angle <= MIN_ANGLE {
-//                 side = true;
-//             }
-//         }
-
-//         thread::sleep(time::Duration::from_millis(DELAY_IN_MILLS));
-//         clearscreen::clear().expect("failed to clear screen");
-
-//     }
-// }
