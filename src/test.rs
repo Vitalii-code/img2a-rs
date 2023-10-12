2154d84628f471a5143de7f52225b8d98c6c768b
Vitalya <58048618+Vitalya-code@users.noreply.github.com>
2023-10-12 08:51:56 +0000

    Better tests

--- src/test.rs
+++ src/test.rs
@@ -1,18 +1,30 @@
+use std::path::Path;
 use image::flat::Error;
 use super::*;
+use rand::prelude::*;
 
 #[test]
 fn my_test() {
-    for i in 1..2000 {
-        generate("imgs/image.png", (i, 500)).unwrap();
-        to_ascii("imgs/image.png");
+    for w in (80..1000).step_by(6) {
+        for h in (80..1000).step_by(8) {
+            let img_path = format!("imgs/{}x{}_image.png", w, h) ; 
+            generate(&img_path, (w, h)).unwrap();
+            println!("{}", img_path);
+            to_ascii(&img_path);
+
+        }
     }
 }
 
 
-fn generate(name: &str, image_size: (u32, u32)) -> Result<(),Error>{
+fn generate(path: &str, image_size: (u32, u32)) -> Result<(),Error>{
     // This function generates an image for testing
+    let mut rng = thread_rng();
 
+    // if image exist then skip
+    if Path::new(path).exists() == true {
+        return Ok(())
+    }
     // Create a new ImgBuf with width: imgx and height: imgy
     let mut imgbuf = image::ImageBuffer::new(image_size.0, image_size.1);
 
@@ -21,12 +33,12 @@ fn generate(name: &str, image_size: (u32, u32)) -> Result<(),Error>{
         for y in 0..image_size.1 {
             let pixel = imgbuf.get_pixel_mut(x, y);
             let image::Rgb(data) = *pixel;
-            *pixel = image::Rgb([data[0], 0 as u8, data[2]]);
+            *pixel = image::Rgb([data[0], rng.gen_range(0..=255) as u8, data[2]]);
         }
     }
 
     // Save the image as “fractal.png”, the format is deduced from the path
-    imgbuf.save(name).unwrap();
+    imgbuf.save(path).unwrap();
     
     Ok(())
 }
